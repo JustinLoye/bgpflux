598fa33b605f0fe6f40878d70d523a507df8dd87
JustinLoye <justin.loye@gmail.com>
2026-04-21 08:49:36 +0000

    support for ris xor rv live

--- src/live.rs
+++ src/live.rs
@@ -2,16 +2,24 @@ use crate::config::validate_collectors;
 use crate::runtime::intern_collector;
 use crate::BgpStreamElem;
 use bgpkit_broker::BrokerError;
+use bgpkit_parser::bmp::messages::BmpMessageBody;
 use bgpkit_parser::models::{Asn, Community, ElemType, MetaCommunity, NetworkPrefix, Origin};
-use bgpkit_parser::BgpElem;
-use bgpkit_parser::Filter;
+use bgpkit_parser::{parse_bmp_msg, parse_openbmp_header, BgpElem};
+use bgpkit_parser::{Elementor, Filter};
 // use bgpkit_parser::parse_ris_live_message;
 use bgpkit_parser::rislive::error::ParserRisliveError;
 use bgpkit_parser::rislive::messages::{
     RisLiveClientMessage, RisLiveMessage, RisMessageEnum, RisSubscribe,
 };
+use bytes::Bytes;
 use ipnet::IpNet;
+use kafka::client::KafkaClient;
+use kafka::consumer::{Consumer, FetchOffset, GroupOffsetStorage};
+use kafka::error::Error as KafkaError;
+use std::collections::VecDeque;
 use std::net::{Ipv4Addr, TcpStream};
+use std::thread::sleep;
+use std::time::Duration;
 use std::{thread, time};
 use tungstenite::stream::MaybeTlsStream;
 use tungstenite::{connect, Message, WebSocket};
@@ -292,27 +300,209 @@ impl LiveConfig {
 
 pub struct LiveBgpStream {
     pub config: LiveConfig,
+    ris_collectors: Vec<String>,
+    rv_collectors: Vec<String>,
+}
+
+fn build_pattern(collectors: &[String]) -> String {
+    let escaped: Vec<String> = collectors.iter().map(|c| regex::escape(c)).collect();
+    let alternation = escaped.join("|");
+    format!(r#"^routeviews\.({})\..*\.bmp_raw$"#, alternation)
+}
+
+struct RouteviewsStream {
+    con: Consumer,
+    buffer: VecDeque<BgpStreamElem>,
+}
+
+fn get_matching_topics(client: &mut KafkaClient, pattern: &str) -> Vec<String> {
+    let re = regex::Regex::new(pattern).expect("Invalid regex pattern");
+    client.load_metadata_all().expect("Failed to load metadata");
+    client
+        .topics()
+        .iter()
+        .filter(|t| re.is_match(t.name())) // Adjust pattern matching as necessary
+        .map(|t| t.name().to_string())
+        .collect()
+}
+
+// This struct is adapted from bgpkit-parser
+// Original source: https://github.com/bgpkit/bgpkit-parser/
+// Copyright (c) 2021 Mingwei Zhang
+// Licensed under the MIT License
+impl RouteviewsStream {
+    pub fn new(group: String, pattern: String, brokers: Vec<String>) -> Result<Self, KafkaError> {
+        let mut client = KafkaClient::new(brokers);
+        client
+            .load_metadata_all()
+            .expect("Failed to connect to Kafka");
+        let topics = get_matching_topics(&mut client, pattern.as_str());
+        dbg!(&topics);
+
+        let mut builder = Consumer::from_client(client);
+        for topic in topics {
+            builder = builder.with_topic(topic);
+        }
+        let con = builder
+            .with_group(group)
+            .with_fetch_max_bytes_per_partition(100_000)
+            .with_retry_max_bytes_limit(1_000_000)
+            .with_fallback_offset(FetchOffset::Latest)
+            .with_offset_storage(Some(GroupOffsetStorage::Kafka))
+            .create()?;
+        Ok(Self {
+            con,
+            buffer: VecDeque::new(),
+        })
+    }
+}
+
+impl Iterator for RouteviewsStream {
+    type Item = BgpStreamElem;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            if let Some(elem) = self.buffer.pop_front() {
+                return Some(elem);
+            }
+
+            let mss = self.con.poll().ok()?;
+            if mss.is_empty() {
+                // println!("No messages available right now, wait for 5 seconds.");
+                sleep(Duration::from_secs(5));
+                continue;
+            }
+
+            for ms in mss.iter() {
+                for m in ms.messages() {
+                    let mut bytes = Bytes::from(m.value.to_vec());
+                    let header = parse_openbmp_header(&mut bytes).unwrap();
+                    let collector = intern_collector(header.router_group.unwrap());
+                    let bmp_msg = parse_bmp_msg(&mut bytes);
+                    match bmp_msg {
+                        Ok(msg) => {
+                            let per_peer_header = msg.per_peer_header.unwrap();
+                            if let BmpMessageBody::RouteMonitoring(m) = msg.message_body {
+                                let timestamp = if per_peer_header.timestamp > 0.0 {
+                                    per_peer_header.timestamp
+                                } else {
+                                    header.timestamp
+                                };
+
+                                for elem in Elementor::bgp_to_elems(
+                                    m.bgp_message,
+                                    timestamp,
+                                    &per_peer_header.peer_ip,
+                                    &per_peer_header.peer_asn,
+                                ) {
+                                    let bgpstreamelem = BgpStreamElem {
+                                        collector_id: collector,
+                                        elem_type: elem.elem_type.into(),
+                                        elem,
+                                    };
+                                    self.buffer.push_back(bgpstreamelem);
+                                }
+                            }
+                        }
+                        Err(_e) => {
+                            continue;
+                        }
+                    }
+                }
+                let _ = self.con.consume_messageset(ms);
+            }
+            self.con.commit_consumed().ok()?;
+        }
+    }
 }
 
 impl LiveBgpStream {
     pub fn new(config: LiveConfig) -> Self {
-        Self { config }
+        let (ris_collectors, rv_collectors): (Vec<String>, Vec<String>) = config
+            .collectors
+            .clone()
+            .into_iter()
+            .partition(|c| c.contains("rrc"));
+        Self {
+            config,
+            ris_collectors,
+            rv_collectors,
+        }
     }
 
-    pub fn build(self) -> impl Iterator<Item = BgpStreamElem> {
-        // connect to RIPE RIS Live websocket server
-        let (mut socket, _response) =
-            connect(RIS_LIVE_URL).expect("Can't connect to RIS Live websocket server");
-
-        for collector in self.config.collectors {
-            let msg = RisSubscribe::new().host(&collector);
-            socket
-                .send(Message::Text(msg.to_json_string().into()))
-                .unwrap();
-            thread::sleep(time::Duration::from_millis(50));
+    pub fn build(self) -> Box<dyn Iterator<Item = BgpStreamElem>> {
+        if !self.ris_collectors.is_empty() & !self.rv_collectors.is_empty() {
+            Box::new(Vec::<BgpStreamElem>::new().into_iter())
         }
+        // Connect to RIPE RIS Live websocket server
+        else if !self.ris_collectors.is_empty() {
+            let (mut socket, _response) =
+                connect(RIS_LIVE_URL).expect("Can't connect to RIS Live websocket server");
 
-        RisStream::new(socket)
+            for collector in self.config.collectors {
+                let msg = RisSubscribe::new().host(&collector);
+                socket
+                    .send(Message::Text(msg.to_json_string().into()))
+                    .unwrap();
+                thread::sleep(time::Duration::from_millis(50));
+            }
+
+            Box::new(RisStream::new(socket))
+        } else if !self.rv_collectors.is_empty() {
+            let broker = "stream.routeviews.org:9092".to_owned();
+
+            let pattern = build_pattern(self.rv_collectors.as_ref());
+
+            // let pattern = r#"routeviews\.amsix\.ams\..*\.bmp_raw"#.to_owned();
+            let group = format!("bgpflux-{}", chrono::Utc::now().timestamp());
+            Box::new(RouteviewsStream::new(group, pattern, vec![broker]).unwrap())
+        } else {
+            Box::new(Vec::<BgpStreamElem>::new().into_iter())
+        }
     }
 }
-// impl
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use chrono::Utc;
+    use std::collections::HashSet;
+
+    /// Consume the live stream to check if the delay is small and if the requested collectors are present
+    fn check_livestream(stream: impl Iterator<Item = BgpStreamElem>, config: LiveConfig) {
+        let test_start = Utc::now().timestamp_micros() as f64 / 1e6;
+        let test_time = 30.0;
+        let max_delay = 30.0;
+
+        let mut seen_collectors = HashSet::new();
+        for elem in stream {
+            let now = Utc::now().timestamp_micros() as f64 / 1e6;
+            if now > test_start + test_time {
+                break;
+            }
+            seen_collectors.insert(elem.collector_id.to_string());
+            assert!(
+                elem.timestamp > now - max_delay,
+                "Live stream as more than {} delay (s)",
+                max_delay as i64
+            );
+        }
+
+        // Check if config and stream match
+        assert_eq!(seen_collectors, HashSet::from_iter(config.collectors));
+    }
+
+    #[test]
+    fn live_ris() {
+        let config = LiveConfig::new(&["rrc00", "rrc21"]).unwrap();
+        let stream = LiveBgpStream::new(config.clone()).build();
+        check_livestream(stream, config);
+    }
+
+    #[test]
+    fn live_rv() {
+        let config = LiveConfig::new(&["route-views2", "amsix.ams"]).unwrap();
+        let stream = LiveBgpStream::new(config.clone()).build();
+        check_livestream(stream, config);
+    }
+}
